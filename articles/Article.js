9f33d1aa945fe4230079d09548b827fd1d6ab278
israelisaque <israelisaque@hotmail.com>
2021-09-24 00:33:03 -0300

    Relacionamentos entre Category e Article

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");
 
 const Article = connection.define('articles', {
   title: {
@@ -15,5 +16,8 @@ const Article = connection.define('articles', {
     allowNull: false
   }
 });
+
+Category.hasMany(Article); // Uma Category tem muitos Articles, relacionamento 1 para muitos
+Article.belongsTo(Category); // Um Article pertence a uma Category, relacionamento 1 para 1 
 
 module.exports = Article;
